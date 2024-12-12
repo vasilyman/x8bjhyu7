9f91575ddf37a1cc48de65b71e24ddf0e15d952b
Butorin Vasiliy <vasilyman@mail.ru>
2024-12-12 20:08:04 +0500

    feat: add start function

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 for rinning the app, please run following commands:
 
+1. install dependencies
+
+`npm i`
+
+2. run app
+
 `npm run start`
--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Hello world!');
+const { debounce } = require("lodash");
+
+const MAX_CALLS = 10;
+
+const printNum = (num) => {
+    console.log(`num of call is ${num}`);
+};
+
+const printNumDebounced = debounce(printNum, 200);
+
+const start = () => {
+    console.log('Hello world!');
+
+    let i = 1;
+
+    const intervalId = setInterval(() => {
+        console.log(`trie of call is ${i}`);
+        printNumDebounced(i);
+
+        if (i >= MAX_CALLS) {
+            clearInterval(intervalId);
+            return;
+        }
+
+        i++;
+    }, 100);
+};
+
+start();
